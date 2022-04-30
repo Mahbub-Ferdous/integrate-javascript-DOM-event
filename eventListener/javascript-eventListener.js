8e9c3fde771e4af8fdd87cf80495a27953f8e744
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2022-04-30 17:13:30 +0600

    some pogram adde and prepare for full folder setup with html css and js

---
+++ eventListener/javascript-eventListener.js
@@ -0,0 +1,5 @@
+/// what is dom events
+
+/* 
+ans is : events means anything happen so when we do interactive our web page by using javascript than we can apply dom event for interactive our button and so on
+*/
